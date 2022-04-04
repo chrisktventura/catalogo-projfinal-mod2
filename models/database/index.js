6e8d6856d24dce48fb3592a77642e105e2515511
Christopher Ventura <chrisventura@outlook.com.br>
2022-04-04 19:45:37 -0300

    deploy heroku

--- models/database/index.js
+++ models/database/index.js
@@ -1,5 +1,14 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize('teste', 'postgres', 'pass', { dialect: 'postgres', host: 'localhost', port: 5432 });
+const sequelize = new Sequelize('teste', 'postgres', 'pass', { dialect: 'postgres', host: 'localhost', port: 5432,  ssl: true,
+protocol: "postgres",
+
+logging: true,
+dialectOptions: {
+  ssl: {
+    require: true,
+    rejectUnauthorized: false
+  }
+} });
 
 module.exports = sequelize;
