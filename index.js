964aa5773a0a2dccea19b2d86415aec9a11e6878
Christopher Ventura <chrisventura@outlook.com.br>
2022-04-04 21:27:36 -0300

    Co-authored-by: Luis Guilherme Kapp <luiskapp@users.noreply.github.com> Co-authored-by: Monica Sumie Hieda Dias <monicahdias@users.noreply.github.com> Co-authored-by: DJMarshallSantos <DJMarshallSantos@users.noreply.github.com>

--- index.js
+++ index.js
@@ -15,31 +15,25 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded());
 app.use(expressLayouts);
 
-let lugar = undefined;
+let lugares = undefined;
 let message = "";
 
 
 app.get("/", async(req, res) => {
   const lugares = await Lugar.findAll();
-  console.log(lugares)
   res.render("index", { lugares });
 });
 
-app.get("/cadastrar", (req, res) => {
-  res.render("cadastrar", catalogo);
-});
+// app.get("/lugares/:id", async (req, res) => {
+//   const lugar = await Lugar.findByPk(req.params.id);
 
-app.post("/formulario", (req, res) => {
-  lugar = req.body;
-  lugar.id = nextId;
-  nextId++;
-  catalogo.push(lugar);
-  lugar = undefined;
-  message = `Seu cadastro foi efetuado com sucesso!`;
-  setTimeout(() => {
-    message = "";
-  }, 1000);
-  res.redirect("/");
+//   res.render("detalhes", {
+//     filme,
+//   });
+// });
+
+app.get("/cadastrar", (req, res) => {
+   res.render("cadastrar", {lugares});
 });
 
 app.get("/editar", (req, res) => {
@@ -53,6 +47,56 @@ app.get("/detalhes", (req, res) => {
 app.get("/sobre", (req, res) => {
   res.render("sobre");
 });
+
+// app.post("/create", async (req, res) => {
+//   const { lugar, frase, descricao, atividades, estrutura, imagem } = req.body;
+  
+//   const lugares = await Lugares.create({
+//     lugar,
+//     frase,
+//     descricao,
+//     atividades,
+//     estrutura,
+//     imagem,
+//   });
+
+//   res.render("cadastro", { lugares });
+    
+//   });
+
+
+
+app.post("/cadastrar", async (req, res) => {
+  const { lugar, frase, descricao, atividades, estrutura, imagem } = req.body;
+  const lugares = await Lugar.create({
+      lugar,
+      frase,
+	descricao,
+	atividades,
+	estrutura,
+      imagem,
+    });
+
+  res.redirect('/', {lugares});
+});
+
+  
+
+
+
+
+// app.post("/formulario", (req, res) => {
+//   lugar = req.body;
+  
+//   catalogo.push(lugar);
+//   lugar = undefined;
+//   message = `Seu cadastro foi efetuado com sucesso!`;
+//   setTimeout(() => {
+//     message = "";
+//   }, 1000);
+//   res.redirect("/");
+// });
+
 
 app.listen(port, () =>
   console.log(`Servidor rodando em http://localhost:${port}`)
