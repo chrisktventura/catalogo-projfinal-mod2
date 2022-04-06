dc74720bb986677989f5ec9add7b1483e344a743
Christopher Ventura <chrisventura@outlook.com.br>
2022-04-05 22:57:00 -0300

    .

--- models/database/index.js
+++ models/database/index.js
@@ -1,12 +1,15 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, { dialect: 'postgres',
-dialectOptions: {
-  ssl:{
-    require: true,
-    rejectUnauthorized: false,
-  }
-}
+const sequelize = new Sequelize('lugares', 'postgres', 'admin', { dialect: 'postgres', host: 'localhost', port: 5432,  ssl: true, 
+// protocol: "postgres",
+// logging: true,
+// dialectOptions: {
+//   ssl: {
+//     require: true,
+//     rejectUnauthorized: false
+//   }
+// }
+ 
   });
 
 module.exports = sequelize;
